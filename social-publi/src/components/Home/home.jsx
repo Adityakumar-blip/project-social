2abff64cc1345af9fb2de95bc0f3e5932c3ddb00
Aditya kumar <adityakamal303@gmail.com>
2022-02-14 12:36:26 +0530

    tools section added

--- social-publi/src/components/Home/home.jsx
+++ social-publi/src/components/Home/home.jsx
@@ -8,6 +8,7 @@ import Clients from './Clients';
 import Footer from '../Footer'; 
 import Letstalk from '../LetsTalk';
 import Newsletter from './Newsletter';
+import {Link} from 'react-router-dom'
 
 
 const Home = () => {
@@ -15,7 +16,7 @@ const Home = () => {
     <>
     <section className='landing-page'>
       <header>
-      <div className="video-container">
+      <div className="col-12 col-s-12 video-container">
         <video src={bgImg} className="video" autoPlay loop muted type="video/mp4">
             </video>
             <div className="main-heading-wrapper">
@@ -23,7 +24,8 @@ const Home = () => {
             <strong>Done <span>Right</span></strong></h1>
             <p className="main-para">As an Influencer Marketing Agency, we create the most viral,<br/> 
             captivating & ROI-focused social media influencer<br/> campaigns for brands globally.</p>
-            <button type="button" class="main-button btn btn-outline-light btn-lg "><strong> Our Services </strong>
+            <button type="button" class="main-button btn btn-outline-light btn-lg ">
+            <Link className='nav-link' to='/services'>Our work</Link>
             </button>
             </div>
             </div>
